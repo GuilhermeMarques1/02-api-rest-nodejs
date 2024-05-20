39caf8cdc8bf3652aac7f07a96a4dbe8dd8a5b03
GuilhermeMarques1 <g-marques@outlook.com>
2024-05-20 00:26:06 -0300

    dev: Test get all transactions

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,4 +1,4 @@
-import { it, beforeAll, afterAll, describe } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect } from 'vitest'
 import supertest from 'supertest'
 import { app } from '../src/app'
 
@@ -20,5 +20,28 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('shoul be able to list all transactions', async () => {
+    const createTransactionRespoonse = await supertest(app.server)
+      .post('/transactions')
+      .send({
+        title: 'new transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionRespoonse.get('Set-Cookie') || []
+
+    const getTrasactionsResponse = await supertest(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    expect(getTrasactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'new transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
